da0f69cdd5aa402f2bef7d671001270ef872f538
liyao <liyao4@xiamenair.com>
2018-06-20 16:44:06 +0800

    first commit

---
+++ src/main/java/com/ly/boomchicken/BoomchickenApplication.java
@@ -0,0 +1,12 @@
+package com.ly.boomchicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomchickenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoomchickenApplication.class, args);
+	}
+}
